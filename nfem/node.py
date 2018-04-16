dbbbeaee6cb4ecbf3ddf30af0b338bdf6614a0e1
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-04-16 10:09:09 +0200

    Add documentation

--- nfem/node.py
+++ nfem/node.py
@@ -1,13 +1,43 @@
-"""FIXME"""
+"""This module only contains the Node class
+"""
 
 import numpy as np
 
 class Node(object):
-    """FIXME"""
+    """Three dimensional Node providing Dofs for displacements.
+
+    Attributes
+    ----------
+    id : int or str
+        Unique ID.
+    reference_x : float
+        Reference X coordinate.
+    reference_y : float
+        Reference Y coordinate.
+    reference_z : float
+        Reference Z coordinate.
+    x : float
+        Actual X coordinate.
+    y : float
+        Actual Y coordinate.
+    z : float
+        Actual Z coordinate.
+    """
 
     def __init__(self, id, x, y, z):
-        """FIXME"""
+        """Create a new node.
 
+        Parameters
+        ----------
+        id : int or str
+            Unique ID of the node.
+        x : float
+            Initial X coordinate of the node.
+        y : float
+            Initial Y coordinate of the node.
+        z : float
+            Initial Z coordinate of the node.
+        """
         self.id = id
         self.x = x
         self.y = y
@@ -16,19 +46,42 @@ class Node(object):
         self.reference_y = y
         self.reference_z = z
 
-    def GetActualLocation(self):
-        """FIXME"""
-
-        return np.array([self.x, self.y, self.z])
-
     def GetReferenceLocation(self):
-        """FIXME"""
+        """Location of the node in the reference configuration.
 
-        return np.array([self.reference_x, self.reference_y, self.reference_z])
+        Returns
+        -------
+        location : ndarray
+            Numpy array containing the reference coordinates X, Y and Z.
+        """
+        x = self.reference_x
+        y = self.reference_y
+        z = self.reference_z
+
+        return np.array([x, y, z], dtype=float)
+
+    def GetActualLocation(self):
+        """Location of the node in the actual configuration.
+
+        Returns
+        -------
+        location : ndarray
+            Numpy array containing the actual coordinates X, Y and Z.
+        """
+        x = self.x
+        y = self.y
+        z = self.z
+
+        return np.array([x, y, z], dtype=float)
 
     def GetDisplacement(self):
-        """FIXME"""
+        """Displacement of the node in the actual configuration.
 
+        Returns
+        -------
+        displacement : ndarray
+            A numpy array containing the displacements u, v and w.
+        """
         return self.GetReferenceLocation() - self.GetActualLocation()
 
     def Update(self, dof_type, value):
