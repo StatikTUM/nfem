4fd31ce51902a210b73715ca713de15c74ae6bc4
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-05-03 10:20:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nfem',
+    version='1.2',
+    description='NFEM Teaching Tools',
+    url='http://gitlab.lrz.de/chair_of_structural_analysis/NFEM_Teaching_Tool',
+    author='Thomas Oberbichler, Armin Geiser, Klaus Sautter, Aditya Ghantasala',
+    author_email='',
+    license='',
+    packages=['nfem'],
+    zip_safe=False
+)
