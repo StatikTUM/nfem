b78833963fd45e2fc425643b1d146493b819af37
arming <armin.geiser@tum.de>
2018-05-08 10:51:29 +0200

    add missing newline

--- nfem/__init__.py
+++ nfem/__init__.py
@@ -1,5 +1,5 @@
 """
-NFEM teaching tool            
+NFEM teaching tool
 
 A module for the non linear static analysis of 3D truss problems.
 A light weight object oriented FEM code and some usefull postprocessing tools.
@@ -12,9 +12,9 @@ print("""
 
                        _   ________________  ___
                       / | / / ____/ ____/  |/  /
-                     /  |/ / /_  / __/ / /|_/ / 
-                    / /|  / __/ / /___/ /  / /  
-                   /_/ |_/_/   /_____/_/  /_/  Teaching Tool              
+                     /  |/ / /_  / __/ / /|_/ /
+                    / /|  / __/ / /___/ /  / /
+                   /_/ |_/_/   /_____/_/  /_/  Teaching Tool
 
   Authors:   Armin Geiser, Aditya Ghantasala, Thomas Oberbichler, Klaus Sautter
   Copyright: © 2018 TUM Statik
@@ -22,7 +22,7 @@ print("""
 
   This is a teaching tool! All results without warranty.
 
---------------------------------------------------------------------------------  
+--------------------------------------------------------------------------------
 """)
 
 if sys.version_info < (3, 5):
