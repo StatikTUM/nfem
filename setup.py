e135bc58bd46964aef83fe47422a1ee61435ed06
Thomas Oberbichler <thomas.oberbichler@tum.de>
2018-06-18 09:43:56 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nfem',
-    version='1.2',
+    version='1.4',
     description='NFEM Teaching Tools',
     url='http://gitlab.lrz.de/chair_of_structural_analysis/NFEM_Teaching_Tool',
     author='Thomas Oberbichler, Armin Geiser, Klaus Sautter, Aditya Ghantasala',
