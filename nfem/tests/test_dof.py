4d6c28f0396a1a7c2c4780d0c3c9fd716517a246
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-03-10 17:49:14 +0100

    Fix names

--- nfem/tests/test_dof.py
+++ nfem/tests/test_dof.py
@@ -9,46 +9,46 @@ def dof():
 
 
 def test_dof_init(dof):
-    assert_equal(dof.ref_value, 5)
-    assert_equal(dof.act_value, 5)
+    assert_equal(dof.reference_value, 5)
+    assert_equal(dof.value, 5)
     assert_equal(dof.delta, 0)
 
 
 def test_dof_increment(dof):
     dof += 1.5
 
-    assert_equal(dof.ref_value, 5)
-    assert_equal(dof.act_value, 6.5)
+    assert_equal(dof.reference_value, 5)
+    assert_equal(dof.value, 6.5)
     assert_equal(dof.delta, 1.5)
 
 
 def test_dof_decrement(dof):
     dof -= 0.5
 
-    assert_equal(dof.ref_value, 5)
-    assert_equal(dof.act_value, 4.5)
+    assert_equal(dof.reference_value, 5)
+    assert_equal(dof.value, 4.5)
     assert_equal(dof.delta, -0.5)
 
 
-def test_dof_ref_value(dof):
-    dof.ref_value = 3
+def test_dof_reference_value(dof):
+    dof.reference_value = 3
 
-    assert_equal(dof.ref_value, 3)
-    assert_equal(dof.act_value, 5)
+    assert_equal(dof.reference_value, 3)
+    assert_equal(dof.value, 5)
     assert_equal(dof.delta, 2)
 
 
-def test_dof_act_value(dof):
-    dof.act_value = 3
+def test_dof_value(dof):
+    dof.value = 3
 
-    assert_equal(dof.ref_value, 5)
-    assert_equal(dof.act_value, 3)
+    assert_equal(dof.reference_value, 5)
+    assert_equal(dof.value, 3)
     assert_equal(dof.delta, -2)
 
 
 def test_dof_delta(dof):
     dof.delta = 3
 
-    assert_equal(dof.ref_value, 5)
-    assert_equal(dof.act_value, 8)
+    assert_equal(dof.reference_value, 5)
+    assert_equal(dof.value, 8)
     assert_equal(dof.delta, 3)
