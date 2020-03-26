b8db0a00629963dad6ddc5dc81f8717c751cd324
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-03-26 22:07:42 +0100

    Fix import

--- nfem/visualization/__init__.py
+++ nfem/visualization/__init__.py
@@ -5,8 +5,13 @@ except Exception as _:
 
 try:
     get_ipython()
-    from nfem.visualization.notebook_animation import show_load_displacement_curve, show_animation, show_deformation_plot
-    from nfem.visualization.notebook_plot import Plot2D
+    _is_notebook = True
 except Exception as _:
+    _is_notebook = False
+
+if _is_notebook:
+    from nfem.visualization.notebook_animation import show_animation, show_deformation_plot
+    from nfem.visualization.notebook_plot import show_load_displacement_curve, Plot2D
+else:
     from nfem.visualization.plot import show_load_displacement_curve, show_animation, show_deformation_plot
     from nfem.visualization.plot import Plot2D
