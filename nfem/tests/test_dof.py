c81ddba539c0e7db134dbb0e774b734894bf3f88
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-03-28 19:31:33 +0100

    Add test

--- nfem/tests/test_dof.py
+++ nfem/tests/test_dof.py
@@ -1,6 +1,7 @@
 import pytest
 from numpy.testing import assert_equal
 from nfem.dof import Dof
+from copy import deepcopy
 
 
 @pytest.fixture
@@ -12,6 +13,16 @@ def test_dof_init(dof):
     assert_equal(dof.reference_value, 5)
     assert_equal(dof.value, 5)
     assert_equal(dof.delta, 0)
+
+
+def test_dof_equality(dof):
+    dof_2 = deepcopy(dof)
+    dof_3 = Dof('test2', 5)
+
+    assert(dof == dof_2)
+    assert(dof == 'test')
+    assert(dof != dof_3)
+    assert(dof != 'test2')
 
 
 def test_dof_reference_value(dof):
