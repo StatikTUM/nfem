b61f73ebd174fa8b0407d5598a650bcf65eba5df
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-03-28 19:20:38 +0100

    Fix tests

--- examples/03_NFEM_2_bar_mechanism.py
+++ examples/03_NFEM_2_bar_mechanism.py
@@ -13,12 +13,10 @@ model = Model('Mechanism')
 
 model.add_node(id='A', x=0, y=0, z=0)
 model.add_node(id='B', x=1, y=0.5, z=0)
-model.add_node(id='C', x=2, y=0, z=0)
+model.add_node(id='C', x=2, y=0, z=0, fx=-1)
 
 model.add_truss_element(id=1, node_a='A', node_b='B', youngs_modulus=1, area=1)
 model.add_truss_element(id=2, node_a='B', node_b='C', youngs_modulus=1, area=1)
-
-model.add_single_load(id='load', node_id='C', fu=-1)
 
 model.add_dirichlet_condition(node_id='A', dof_types='uvw', value=0)
 model.add_dirichlet_condition(node_id='B', dof_types='w', value=0)
--- nfem/tests/test_two_bar_truss_model.py
+++ nfem/tests/test_two_bar_truss_model.py
@@ -10,13 +10,11 @@ class TestModel(TestCase):
         self.model = Model('Two-Bar Truss')
 
         self.model.add_node(id='A', x=0, y=0, z=0)
-        self.model.add_node(id='B', x=1, y=1, z=0)
+        self.model.add_node(id='B', x=1, y=1, z=0, fy=-1)
         self.model.add_node(id='C', x=2, y=0, z=0)
 
         self.model.add_truss_element(id=1, node_a='A', node_b='B', youngs_modulus=1, area=1)
         self.model.add_truss_element(id=2, node_a='B', node_b='C', youngs_modulus=1, area=1)
-
-        self.model.add_single_load(id='load 1', node_id='B', fv=-1)
 
         self.model.add_dirichlet_condition(node_id='A', dof_types='uvw', value=0)
         self.model.add_dirichlet_condition(node_id='B', dof_types='w', value=0)
@@ -29,7 +27,7 @@ class TestModel(TestCase):
 
     def test_elements_container(self):
         actual = len(self.model.elements) 
-        expected = 3
+        expected = 2
         assert(actual == expected)
 
 def get_model():
