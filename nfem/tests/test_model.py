26d5a8910e819a672c41ae490b7397796a3ed9bf
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-04-02 11:08:56 +0200

    Add test

--- nfem/tests/test_model.py
+++ nfem/tests/test_model.py
@@ -34,6 +34,11 @@ def test_integer_node_key_raises(model):
         model.add_node(id=9, x=0, y=0, z=0)
 
 
+def test_duplicate_node_key_raises(model):
+    with pytest.raises(KeyError):
+        model.add_node(id='A', x=0, y=0, z=0)
+
+
 def test_integer_truss_key_raises(model):
     with pytest.raises(TypeError):
         model.add_truss(id=9, node_a='A', node_b='B', youngs_modulus=1, area=1)
@@ -43,3 +48,8 @@ def test_integer_truss_key_raises(model):
 
     with pytest.raises(TypeError):
         model.add_truss(id='9', node_a='A', node_b=1, youngs_modulus=1, area=1)
+
+
+def test_duplicate_truss_key_raises(model):
+    with pytest.raises(KeyError):
+        model.add_truss(id='1', node_a='A', node_b='B', youngs_modulus=1, area=1)
