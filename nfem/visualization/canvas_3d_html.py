717811b96c28ce7f087ce46cb3df4ed830749fc5
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-11-20 10:04:34 +0100

    Add content div

--- nfem/visualization/canvas_3d_html.py
+++ nfem/visualization/canvas_3d_html.py
@@ -15,10 +15,12 @@ TEMPLATE = """
 }
 </style>
 
-<div id="caption" style="position: absolute; width: 100%; text-align: center;">Title</div>
-<input id="timestep" type="range" min="0" max="10" value="0" step="1" class="slider" style="position: absolute; bottom: 0px; width: 100%;">
+<div id="content">
+    <div id="caption" style="position: absolute; width: 100%; text-align: center;">Title</div>
+    <input id="timestep" type="range" min="0" max="10" value="0" step="1" class="slider" style="position: absolute; bottom: 0px; width: 100%;">
 
-<div id="container"></div>
+    <div id="container"></div>
+</div>
 
 <script src="https://unpkg.com/three@0.119.1/build/three.min.js"></script>
 <script src="https://unpkg.com/three@0.119.1/examples/js/controls/OrbitControls.js"></script>
