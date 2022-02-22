3260c1c08a71f2d57ba6e0e2b556b052f831584e
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2022-02-22 14:51:32 +0100

    Simplify

--- nfem/visualization/__init__.py
+++ nfem/visualization/__init__.py
@@ -1,12 +1,2 @@
-try:
-    get_ipython()
-    _is_notebook = True
-except Exception as _:
-    _is_notebook = False
-
-if _is_notebook:
-    from nfem.visualization.notebook_animation import show_animation, show_deformation_plot
-    from nfem.visualization.notebook_plot import show_load_displacement_curve, Plot2D
-else:
-    from nfem.visualization.plot import show_load_displacement_curve, show_animation, show_deformation_plot
-    from nfem.visualization.plot import Plot2D
+from nfem.visualization.notebook_animation import show_animation, show_deformation_plot
+from nfem.visualization.notebook_plot import show_load_displacement_curve, Plot2D
