3ba0d8c63045dff848401f78df558aaede0cc0ee
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2022-02-25 08:59:00 +0100

    Create viewer.py

---
+++ nfem/viewer.py
@@ -0,0 +1,65 @@
+from IPython.display import display_html
+from xml.dom.minidom import parse as parse_xml
+import json
+import pathlib
+import html
+
+
+ROOT = pathlib.Path(__file__).parent.resolve()
+
+
+class Viewer:
+    def html(self, models, height):
+        timesteps = []
+
+        for model in models:
+            timestep = {}
+
+            timestep['name'] = model.name or ''
+
+            timestep['objects'] = objects = []
+
+            for element in model.elements:
+                element.draw(objects)
+
+            for node in model.nodes:
+                node.draw(objects)
+
+            timesteps.append(timestep)
+
+        data = dict(
+            settings=dict(
+                height=height,
+            ),
+            timesteps=timesteps,
+        )
+
+        return _load_with_data(f'{ROOT}/html/viewer.html', data)
+
+    def frame(self, models, height):
+        html_content = self.html(models, height)
+
+        return _iframe(html_content, height)
+
+
+def _iframe(content: str, height: int):
+    return f'<iframe seamless frameborder="0" allowfullscreen width="100%" height="{height}" srcdoc="{html.escape(content)}"></iframe>'
+
+
+def _load_with_data(path, data):
+    with open(path, 'r') as f:
+        html = f.read()
+
+    start_tag = '<script id="data" type="application/json">'
+    end_tag = '</script>'
+
+    start_index = html.find(start_tag) + len(start_tag)
+
+    head = html[:start_index]
+    tail = html[start_index:]
+
+    end_index = html.find(end_tag)
+
+    tail = html[end_index:]
+
+    return head + json.dumps(data) + tail
